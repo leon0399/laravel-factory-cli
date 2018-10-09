bd7c2712f3b40814f4778f18ca254439f5fef4c2
Leonid Meleshin <leon.03.99@gmail.com>
2018-10-09 18:58:10 +0300

    Initial project setup

---
+++ src/FactoryCLIServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Leon0399\Laravel\FactoryCLI;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class FactoryCLIServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
